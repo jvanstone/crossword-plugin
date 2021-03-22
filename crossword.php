76bd9ede1e771f08fc0736782fac82b23af2462d
jvanstone <jason@vanstoneonline.com>
2021-03-22 13:17:24 -0400

    remove extra end div

--- crossword.php
+++ crossword.php
@@ -3,7 +3,7 @@
 Plugin Name: Crossword Plugin - Canada Info Add-ON
 Plugin URI: 
 Description: Adds a plug-in to the Canada Info
-Version: 1
+Version: 1.0
 Author: Vanstone Online
 Author URI: https://vanstoneonline.com
 Text Domain: cg-crossword
@@ -19,7 +19,7 @@ Text Domain: cg-crossword
  * @link https://developer.wordpress.org/reference/functions/wp_enqueue_script/
  */
 function cg_crossword_plugin_css() {
-    wp_enqueue_style( 'cg_crossword', plugin_dir_url(__FILE__).'assets/css/crossword.css' );
+	wp_enqueue_style( 'cg_crossword', plugin_dir_url(__FILE__).'assets/css/crossword.css' );
 }
 add_action( 'wp_enqueue_scripts', 'cg_crossword_plugin_css', 999 );
 
@@ -33,7 +33,7 @@ add_action( 'wp_enqueue_scripts', 'cg_crossword_plugin_css', 999 );
  */
 function cg_crossword_scripts() {
  
-    wp_enqueue_script( 'CG-CW1', plugin_dir_url(__FILE__).'assets/js/crossword2.js', array(), null, true);
+	wp_enqueue_script( 'CG-CW1', plugin_dir_url(__FILE__) . 'assets/js/crossword2.js', array(), null, true);
  
 
 }
@@ -43,48 +43,48 @@ add_action( 'wp_enqueue_scripts', 'cg_crossword_scripts', 999 );
 
 
 if( !function_exists('cg_crossword')) {
-    function cg_crossword() { 
-    
-    //Bulk Add HTML to the Function.
-    ob_start(); ?>
-    </article>
-    <article style="margin-top:-8rem;">
-    <div id="printableArea">
-        <h2 class="printOnly">Canada Info Guide - Issue 1 Crossword </h2>
-        <div id="puzzle_container" class="container-fluid">
-            <table id="puzzle" class="table-responsive">
-            </table>
+	function cg_crossword() { 
+	
+	//Bulk Add HTML to the Function.
+	ob_start(); ?>
+	</article>
+	<article style="margin-top:-8rem;">
+	<div id="printableArea">
+		<h2 class="printOnly">Canada Info Guide - Issue 1 Crossword </h2>
+		<div id="puzzle_container" class="container-fluid">
+			<table id="puzzle" class="table-responsive">
+			</table>
 
-            <div id="puzzle-img">
-                <p> Please note: the Digital Crossword puzzle currently works only with iPads and Desktops. You are more then welcome to print the crossword out if you wish.</p>
+			<div id="puzzle-img">
+				<p> Please note: the Digital Crossword puzzle currently works only with iPads and Desktops. You are more then welcome to print the crossword out if you wish.</p>
 
-                <img src="<?php echo plugin_dir_url(__FILE__); ?>assets/img/crossword.png" class="img-fluid" alt="crossword preview" />
-            </div>
+				<img src="<?php echo plugin_dir_url(__FILE__); ?>assets/img/crossword.png" class="img-fluid" alt="crossword preview" />
+			</div>
 
-            <div id="buttons_container" class="col-12 d-flex flex-wrap justify-content-around">
-                <button id="clue" class="mobile-hide">Clue</button>
-                <button id="check" class="mobile-hide">Check</button>
-                <button id="solve" class="mobile-hide">Solve</button>
-                <button onclick="window.print();return false;" class="mobile-show" />Print Me</button>
-                <button id="clear_all" class="mobile-hide">Clear All</button>
-            </div>
+			<div id="buttons_container" class="col-12 d-flex flex-wrap justify-content-around">
+				<button id="clue" class="mobile-hide">Clue</button>
+				<button id="check" class="mobile-hide">Check</button>
+				<button id="solve" class="mobile-hide">Solve</button>
+				<button onclick="window.print();return false;" class="mobile-show" />Print Me</button>
+				<button id="clear_all" class="mobile-hide">Clear All</button>
+			</div>
 
-            <div id="hints_container" class="col-12 d-flex flex-wrap">
-                <div class="card col-md-5 hints">
-                    <div class="header"><h3>Vertical</h3></div>
-                    <div id="vertical_hints_container" class="card-body"></div>
-                </div>
-                <div class="card col-md-5 hints"> 
-                    <div class="header"><h3>Horizontal</h3></div>
-                    <div id="horizontal_hints_container" class="card-body"></div>
-                </div>
-            </div>
-        </div>  
-    </div>
+			<div id="hints_container" class="col-12 d-flex flex-wrap">
+				<div class="card col-md-5 hints">
+					<div class="header"><h3>Vertical</h3></div>
+					<div id="vertical_hints_container" class="card-body"></div>
+				</div>
+				<div class="card col-md-5 hints"> 
+					<div class="header"><h3>Horizontal</h3></div>
+					<div id="horizontal_hints_container" class="card-body"></div>
+				</div>
+			</div>
+		</div>  
+	
 
-    <?php
-        return ob_get_clean();
-    }
+		<?php
+		return ob_get_clean();
+	}
 }
 
 add_shortcode('crossword1', 'cg_crossword'); 
